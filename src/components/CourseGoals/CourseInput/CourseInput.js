0a9ef37c56be77ca4f334bfe186820d02c15aa60
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-06-07 15:06:49 +0300

    Adding wrong input styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,33 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0) {
+      setValid(true);
+    }
+
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <label>Course Goal</label>
+      <input type="text" onChange={goalInputChangeHandler} />
+        {/* <label style={{ color: !isValid ? 'red' : 'black'}}>Course Goal</label> */}
+        {/* <input style={{ borderColor: !isValid ? 'red' : 'black', background: !isValid ? 'salmon' : 'transparent'}} type="text" onChange={goalInputChangeHandler} /> */}
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
